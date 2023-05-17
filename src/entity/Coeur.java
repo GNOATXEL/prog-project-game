8c9c414146b2fe9dbd795006b58719911953369f
Ivann Vyslanko <ivann.vyslanko@etudiant.univ-rennes1.fr>
2023-05-17 13:29:14 +0200

    supprimé des trucs qui servent à rine dans le ❤️‍🔥

--- src/entity/Coeur.java
+++ src/entity/Coeur.java
@@ -3,8 +3,6 @@ package entity;
 import main.GamePanel;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -37,18 +35,11 @@ public class Coeur extends UnlivingEntity {
     }
 
     public void getCoeurImage() {
-        //gestion des exceptions
+        // gestion des exceptions
         try {
             m_idleImage = ImageIO.read(new File("res/tiles/COEURPLEIN.png"));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Image non trouvée");
         }
-    }
-
-    public void draw(Graphics2D a_g2) {
-        // récupère l'image du joueur
-        BufferedImage l_image = m_idleImage;
-        // affiche le personnage avec l'image "image", avec les coordonnées x et y, et de taille tileSize (16x16) sans échelle, et 48x48 avec échelle)
-        a_g2.drawImage(l_image, position.getX(), position.getY(), m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
     }
 }
